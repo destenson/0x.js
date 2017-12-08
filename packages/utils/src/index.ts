cbe3f20266bd9f5b263a6f3e01f8c9f668af80a8
Leonid Logvinov <logvinov.leon@gmail.com>
2017-12-08 18:01:40 +0300

    Move more shared utils into utils package and reuse them

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,1 +1,4 @@
 export {promisify} from './promisify';
+export {classUtils} from './class_utils';
+export {intervalUtils} from './interval_utils';
+export {bigNumberConfigs} from './bignumber_config';
