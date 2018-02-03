56589a8fb658e3de296a961ad737f1322f2f0d15
Remco Bloemen <remco@wicked.ventures>
2018-02-02 16:56:18 -0800

    Add EIP721 standard.

--- packages/contracts/contracts/interfaces/IEIP721.sol
+++ packages/contracts/contracts/interfaces/IEIP721.sol
@@ -1,113 +1,89 @@
 pragma solidity ^0.4.19;
 
-import 'IEIP20Optional.sol';
+import './IEIP165.sol';
 
-// https://github.com/ethereum/EIPs/issues/821
-contract IEIP821 is IEIP20Optional {
+/// @title Interface for contracts conforming to ERC-721: Deed Standard
+/// @author William Entriken (https://phor.net), et. al.
+/// @dev Specification at https://github.com/ethereum/eips/XXXFinalUrlXXX
+contract IEIP721 is IEIP165 {
+
+    // @dev ERC-165 (draft) interface signature for ERC721
+    bytes4 internal constant INTERFACE_ID = // 0xda671b9b
+        bytes4(keccak256('ownerOf(uint256)')) ^
+        bytes4(keccak256('countOfDeeds()')) ^
+        bytes4(keccak256('countOfDeedsByOwner(address)')) ^
+        bytes4(keccak256('deedOfOwnerByIndex(address,uint256)')) ^
+        bytes4(keccak256('approve(address,uint256)')) ^
+        bytes4(keccak256('takeOwnership(uint256)'));
     
-    bytes4 constant INTERFACE_ID =
-        IEIP20Optional.INTERFACE_ID ^
-        bytes4(keccak256('isERC821()')) ^
-        bytes4(keccak256('totalSupply()')) ^
-        bytes4(keccak256('balanceOf(address)')) ^
-        bytes4(keccak256('transfer(address,uint256,bytes)')) ^
-        bytes4(keccak256('approve(address,uint256,bytes)')) ^
-        bytes4(keccak256('transferFrom(address,address,uint256,bytes)'));
-    
-    function IEIP821()
+    function IEIP721()
         public
     {
         supportsInterface[INTERFACE_ID] = true;
     }
-    
-    event Transfer(
-        address indexed from,
-        address indexed to,
-        uint256 indexed assetId,
-        address operator,
-        bytes userData,
-        bytes operatorData
-    );
-    event Update(
-        uint256 indexed assetId,
-        address indexed owner,
-        address indexed operator,
-        string data
-    );
-    event AuthorizeOperator(
-        address indexed operator,
-        address indexed owner,
-        bool authorized
-    );
-    
-    function description()
-        public view
-        returns (string);
-    
-    function totalSupply()
-        public view
-        returns (uint256);
-    
-    function isERC821()
-        public pure
-        returns (bool)
-    {
-        return true;
-    }
 
-    function exists(uint256 assetId)
-        public view
-        returns (bool);
-    
-    function ownerOf(uint256 assetId)
-        public view
-        returns (address);
-    
-    function safeOwnerOfOf(uint256 assetId)
-        public view
-        returns (address);
-    
-    function assetData(uint256 assetId)
-        public view
-        returns (string);
-    
-    function safeAssetData(uint256 assetId)
-        public view
-        returns (string);
+    // PUBLIC QUERY FUNCTIONS //////////////////////////////////////////////////
 
-    function balanceOf(address owner)
-        public view
-        returns (uint256);
-    
-    function assetCount(address owner)
-        public view
-        returns (uint256);
-    
-    function assetByIndex(address owner, uint256 index)
-        public view
-        returns (uint256);
-    
-    function assetsOf(address owner)
-        external view
-        returns (uint256[]);
-    
-    function isOperatorAuthorizedFor(address operator, address assetOwner)
-        public view
-        returns (bool);
+    /// @notice Find the owner of a deed
+    /// @param _deedId The identifier for a deed we are inspecting
+    /// @dev Deeds assigned to zero address are considered invalid, and
+    ///  queries about them do throw.
+    /// @return The non-zero address of the owner of deed `_deedId`, or `throw`
+    ///  if deed `_deedId` is not tracked by this contract
+    function ownerOf(uint256 _deedId) external view returns (address _owner);
 
-    function transfer(
-        address to,
-        uint256 assetId,
-        bytes userData,
-        bytes operatorData)
-        public;
-        
-    function transfer(address to, uint256 assetId, bytes userData)
-        public;
-        
-    function transfer(address to, uint256 assetId)
-        public;
+    /// @notice Count deeds tracked by this contract
+    /// @return A count of valid deeds tracked by this contract, where each one of
+    ///  them has an assigned and queryable owner not equal to the zero address
+    function countOfDeeds() external view returns (uint256 _count);
 
-    function authorizeOperator(address operator, bool authorized)
-        public;
+    /// @notice Count all deeds assigned to an owner
+    /// @dev Throws if `_owner` is the zero address, representing invalid deeds.
+    /// @param _owner An address where we are interested in deeds owned by them
+    /// @return The number of deeds owned by `_owner`, possibly zero
+    function countOfDeedsByOwner(address _owner) external view returns (uint256 _count);
+
+    /// @notice Enumerate deeds assigned to an owner
+    /// @dev Throws if `_index` >= `countOfDeedsByOwner(_owner)` or if
+    ///  `_owner` is the zero address, representing invalid deeds.
+    /// @param _owner An address where we are interested in deeds owned by them
+    /// @param _index A counter less than `countOfDeedsByOwner(_owner)`
+    /// @return The identifier for the `_index`th deed assigned to `_owner`,
+    ///   (sort order not specified)
+    function deedOfOwnerByIndex(address _owner, uint256 _index) external view returns (uint256 _deedId);
+
+    // TRANSFER MECHANISM //////////////////////////////////////////////////////
+
+    /// @dev This event emits when ownership of any deed changes by any
+    ///  mechanism. This event emits when deeds are created (`from` == 0) and
+    ///  destroyed (`to` == 0). Exception: during contract creation, any
+    ///  transfers may occur without emitting `Transfer`. At the time of any transfer,
+    ///  the "approved taker" is implicitly reset to the zero address.
+    event Transfer(address indexed from, address indexed to, uint256 indexed deedId);
+
+    /// @dev The Approve event emits to log the "approved taker" for a deed -- whether
+    ///  set for the first time, reaffirmed by setting the same value, or setting to  
+    ///  a new value. The "approved taker" is the zero address if nobody can take the
+    ///  deed now or it is an address if that address can call `takeOwnership` to attempt
+    ///  taking the deed. Any change to the "approved taker" for a deed SHALL cause
+    ///  Approve to emit. However, an exception, the Approve event will not emit when
+    ///  Transfer emits, this is because Transfer implicitly denotes the "approved taker"
+    ///  is reset to the zero address.
+    event Approval(address indexed owner, address indexed approved, uint256 indexed deedId);
+
+    /// @notice Set the "approved taker" for your deed, or revoke approval by
+    ///  setting the zero address. You may `approve` any number of times while
+    ///  the deed is assigned to you, only the most recent approval matters. Emits
+    ///  an Approval event.
+    /// @dev Throws if `msg.sender` does not own deed `_deedId` or if `_to` ==
+    ///  `msg.sender` or if `_deedId` is not a valid deed.
+    /// @param _deedId The deed for which you are granting approval
+    function approve(address _to, uint256 _deedId) external payable;
+
+    /// @notice Become owner of a deed for which you are currently approved
+    /// @dev Throws if `msg.sender` is not approved to become the owner of
+    ///  `deedId` or if `msg.sender` currently owns `_deedId` or if `_deedId is not a
+    ///  valid deed.
+    /// @param _deedId The deed that is being transferred
+    function takeOwnership(uint256 _deedId) external payable;
 }
